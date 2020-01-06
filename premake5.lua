e8b4915753b13b12971fb0d0206fbf3ffe86460f
Vinícius Jabes <vinijabes@gmail.com>
2020-01-06 08:45:38 -0300

    Premake file

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "lua"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+    
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{        
+        "**.h",
+        "**.c"
+    }
+
+    excludes
+    {
+        "testes/**.h",
+        "testes/**.c",
+    }
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        staticruntime "On"
+            
+	filter "system:windows"
+        systemversion "latest"        
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
